2b36762b9534dd40b9e3862b6e01446f8e4f7b22
Maria Jose Villarreal <villarrealmariaj@gmail.com>
2023-11-04 21:18:57 -0300

    new service error validations

--- utils/validations.ts
+++ utils/validations.ts
@@ -18,3 +18,34 @@ export const comparePassword = (password: string, confirmPassword: string): stri
     }
     return undefined;
 }
+
+export const validateTitleLength = (title: string): string | undefined => {
+    if (title.length < 5) {
+        return 'El título debe tener al menos 5 caracteres';
+    }
+    return undefined;
+}
+
+export const validateShortDescriptionLength = (shortDescription: string): string | undefined => {
+    const wordCount = shortDescription.split(/\s+/).length;
+    if (wordCount < 2 || wordCount > 30) {
+        return 'La descripción corta debe tener más de 1 palabra y 30 palabras o menos.';
+    }
+    return undefined;
+}
+
+export const validateLongDescriptionLength = (longDescription: string): string | undefined => {
+    const wordCount = longDescription.split(/\s+/).length;
+    if ( wordCount < 30 || wordCount > 100) {
+        return 'La descripción larga debe tener más de 30 palabras y 100 palabras o menos.';
+    }
+    return undefined;
+}
+
+export const validatePrice = (price: string): string | undefined => {
+    const priceAsNumber = parseFloat(price);
+    if (priceAsNumber === 0) {
+        return 'El precio debe ser distinto de 0.';
+    }
+    return undefined;
+}
