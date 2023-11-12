6382f0923f0b1ddd4608576488b6adcfa12fb241
Martina Gattas <martigattas@hotmail.com>
2023-11-11 21:22:05 -0300

    add global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,7 @@
-import { ClientsHero } from 'eventapp/components/home/ClientsHero';
-import { Layout } from 'eventapp/components/layout/Layout';
-import Main from 'eventapp/components/main/Main';
-import ProviderList from 'eventapp/components/providerList/ProviderList';
 import { NextPage } from 'next';
 import Head from 'next/head';
 
 const Home: NextPage = () => {
-  const providers = [
-    {
-      title: 'Proveedor 1',
-      imageSrc: 'url',
-      description: 'Descripción del proveedor 1',
-    },
-    {
-      title: 'Proveedor 2',
-      imageSrc: 'url',
-      description: 'Descripción del proveedor 2',
-    },
-    {
-      title: 'Proveedor 3',
-      imageSrc: 'url',
-      description: 'Descripción del proveedor 1',
-    },
-    {
-      title: 'Proveedor 4',
-      imageSrc: 'url',
-      description: 'Descripción del proveedor 2',
-    },
-  ];
   return (
     <>
       <Head>
@@ -45,12 +19,9 @@ const Home: NextPage = () => {
         ></meta>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
-      <Layout variant='general'>{/* <ClientsHero/> */}</Layout>
-      <Main />
-      <ProviderList providers={providers} />
+      <h1>App acá</h1>
     </>
-  );
-};
+  )
+}
 
 export default Home;
